e531c6709d1ba710747453ae0c42ec0b2d91f62f
brightchul <abhbtbb1@gmail.com>
2021-02-28 04:09:23 +0900

    chore: add CssBaseline

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { GlobalStyle, theme } from "./asset/style";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <GlobalStyle />
       <App />
     </ThemeProvider>
