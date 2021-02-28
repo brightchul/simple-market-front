d1317fb6eea79626d16c9ea2ada94bc0b9cc909f
brightchul <abhbtbb1@gmail.com>
2021-02-28 19:32:08 +0900

    add: error404

--- frontend/src/page/error/Error404.tsx
+++ frontend/src/page/error/Error404.tsx
@@ -1,13 +1,27 @@
 import { Main } from "../../layout";
 import React from "react";
+import MyContainer from "component/mycontainer/MyContainer";
+import { MyText } from "component";
+import styled from "styled-components";
 
 const Error404: React.FC = () => {
   return (
     <Main>
-      <div>요청하신 페이지는 없는 페이지 입니다. 404</div>
+      <MyContainer>
+        <Error404Wrapper>
+          <MyText myFont="20pt-600">
+            요청하신 페이지는 없는 페이지 입니다.
+          </MyText>
+        </Error404Wrapper>
+      </MyContainer>
     </Main>
   );
 };
+
+const Error404Wrapper = styled.div`
+  margin-top: 120px;
+  text-align: center;
+`;
 
 export { Error404 };
 export default Error404;
