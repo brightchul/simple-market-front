dc55c908c9f9acc39130da9505d7025518c7c782
brightchul <abhbtbb1@gmail.com>
2021-03-07 16:12:39 +0900

    refactor : MyFont 관련 스타일을 theme에 정리
    테마중 font 관련 스타일은 는 MyText에 정리되어 있었는데 theme 안으로 옮겨서 한곳에서 관리하기 용이하게 변경함

--- frontend/src/asset/style/theme.tsx
+++ frontend/src/asset/style/theme.tsx
@@ -1,6 +1,6 @@
 import { DefaultTheme } from "styled-components";
 
-interface MyFontColor {
+interface MyColor {
   primary700: string;
   primary500: string;
   gray000: string;
@@ -9,8 +9,29 @@ interface MyFontColor {
   warning: string;
 }
 
+interface FontProps {
+  fontFamily: string;
+  fontweight: string;
+  fontStyle?: string;
+  fontSize: string;
+  lineHeight: string;
+}
+
+interface MyFont {
+  "12pt-400": FontProps;
+  "12pt-500": FontProps;
+  "14pt-400": FontProps;
+  "14pt-500": FontProps;
+  "14pt-600": FontProps;
+  "16pt-400": FontProps;
+  "16pt-600": FontProps;
+  "20pt-600": FontProps;
+  "roboto-12-500": FontProps;
+}
+
 interface MyTheme extends DefaultTheme {
-  color: MyFontColor;
+  color: MyColor;
+  font: MyFont;
 }
 
 const theme: MyTheme = {
@@ -22,8 +43,68 @@ const theme: MyTheme = {
     gray900: "#323D45",
     warning: "#FFA000",
   },
+  font: {
+    "12pt-400": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "400",
+      fontSize: "12px",
+      lineHeight: "20px",
+    },
+    "12pt-500": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "500",
+      fontSize: "12px",
+      lineHeight: "20px",
+    },
+
+    "14pt-400": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "400",
+      fontSize: "14px",
+      lineHeight: "20px",
+    },
+    "14pt-500": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "500",
+      fontSize: "14px",
+      lineHeight: "20px",
+    },
+    "14pt-600": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "600",
+      fontSize: "14px",
+      lineHeight: "20px",
+    },
+
+    "16pt-400": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "400",
+      fontSize: "16px",
+      lineHeight: "24px",
+    },
+    "16pt-600": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "600",
+      fontSize: "16px",
+      lineHeight: "24px",
+    },
+
+    "20pt-600": {
+      fontFamily: "Noto Sans KR",
+      fontweight: "600",
+      fontSize: "20px",
+      lineHeight: "32px",
+    },
+
+    "roboto-12-500": {
+      fontFamily: "Roboto",
+      fontweight: "500",
+      fontSize: "12px",
+      lineHeight: "14.06px",
+    },
+  },
 };
 
-export type { MyTheme, MyFontColor };
+export type { MyTheme, MyColor, MyFont };
 export { theme };
 export default theme;
--- frontend/src/component/mytext/index.ts
+++ frontend/src/component/mytext/index.ts
@@ -1,102 +1,17 @@
 import styled from "styled-components";
-import { MyFontColor, theme } from "../../asset/style";
-
-interface FontProps {
-  fontFamily: string;
-  fontweight: string;
-  fontStyle?: string;
-  fontSize: string;
-  lineHeight: string;
-}
-
-interface ThemeFont {
-  "12pt-400": FontProps;
-  "12pt-500": FontProps;
-
-  "14pt-400": FontProps;
-  "14pt-500": FontProps;
-  "14pt-600": FontProps;
-
-  "16pt-400": FontProps;
-  "16pt-600": FontProps;
-
-  "20pt-600": FontProps;
-
-  "roboto-12-500": FontProps;
-}
-
-export const myFontStyle: ThemeFont = {
-  "12pt-400": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "400",
-    fontSize: "12px",
-    lineHeight: "20px",
-  },
-  "12pt-500": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "500",
-    fontSize: "12px",
-    lineHeight: "20px",
-  },
-
-  "14pt-400": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "400",
-    fontSize: "14px",
-    lineHeight: "20px",
-  },
-  "14pt-500": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "500",
-    fontSize: "14px",
-    lineHeight: "20px",
-  },
-  "14pt-600": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "600",
-    fontSize: "14px",
-    lineHeight: "20px",
-  },
-
-  "16pt-400": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "400",
-    fontSize: "16px",
-    lineHeight: "24px",
-  },
-  "16pt-600": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "600",
-    fontSize: "16px",
-    lineHeight: "24px",
-  },
-
-  "20pt-600": {
-    fontFamily: "Noto Sans KR",
-    fontweight: "600",
-    fontSize: "20px",
-    lineHeight: "32px",
-  },
-
-  "roboto-12-500": {
-    fontFamily: "Roboto",
-    fontweight: "500",
-    fontSize: "12px",
-    lineHeight: "14.06px",
-  },
-};
+import { MyColor, MyFont } from "../../asset/style";
 
 interface MyTextProps {
-  myColor?: keyof MyFontColor;
-  myFont: keyof ThemeFont;
+  myColor?: keyof MyColor;
+  myFont: keyof MyFont;
 }
 
 const MyText = styled.div<MyTextProps>`
-  color: ${({ myColor = "gray900" }) => theme.color[myColor]};
-  font-family: ${({ myFont }) => myFontStyle[myFont].fontFamily};
-  font-weight: ${({ myFont }) => myFontStyle[myFont].fontweight};
-  font-size: ${({ myFont }) => myFontStyle[myFont].fontSize};
-  line-height: ${({ myFont }) => myFontStyle[myFont].lineHeight};
+  color: ${({ theme, myColor = "gray900" }) => theme.color[myColor]};
+  font-family: ${({ theme, myFont }) => theme.font[myFont].fontFamily};
+  font-weight: ${({ theme, myFont }) => theme.font[myFont].fontweight};
+  font-size: ${({ theme, myFont }) => theme.font[myFont].fontSize};
+  line-height: ${({ theme, myFont }) => theme.font[myFont].lineHeight};
 `;
 
 export { MyText };
